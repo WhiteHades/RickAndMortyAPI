0370e05841079f5fbb25ffd02a0a89fa22a0cb3f
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2024-02-04 20:14:19 +0100

    Updated the service to fetch the characters in said url.

--- src/app/rick-andmorty.service.ts
+++ src/app/rick-andmorty.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RickAndmortyService {
+  private url = 'https://rickandmortyapi.com/api/character';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getChars(page: number = 1): Observable<any> {
+    return this.http.get(`${this.url}?page=${page}`).pipe(
+      map(response => response)
+    );
+  }
 }
