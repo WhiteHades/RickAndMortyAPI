c6dc81d397fc68003d2d74baefe1bddd45e2d4db
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2024-02-04 21:03:46 +0100

    Working on home ts and home html.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,12 +8,24 @@ import { RickAndMortyServ } from '../rick-andmorty.service';
 })
 export class HomeComponent implements OnInit{
   presentPg = 1;
-  TotalPg = 0;
+  totalPg = 0;
   chars = [];
+  pageNumbers: number[] = [];
 
   constructor(private rickAndMortyServ: RickAndMortyServ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { this.loadChars(); }
+
+  loadChars(): void {
+    this.rickAndMortyServ.getChars(this.presentPg).subscribe(response => {
+      this.chars = response.results;
+      this.totalPg = response.info.pages;
+      this.pageNumbers = Array.from({length: this.totalPg}, (_, i) => i + 1);
+    });
   }
 
+  next(page: number): void {
+    this.presentPg = page;
+    this.loadChars();
+  }
 }
