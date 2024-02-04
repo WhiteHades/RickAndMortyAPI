b30f433a52f6a0a50d874cc3d354716ce3a5ba0b
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2024-02-05 00:19:28 +0100

    Working on the pagination, trying a different, cleaner approach. So far, it's working.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,21 +19,13 @@ export class HomeComponent implements OnInit{
   ngOnInit(): void { this.loadChars(); }
 
   loadChars(): void {
-    this.rickAndMortyServ.getCharacters(this.presentPg).subscribe({
-      next: (response: ApiResponse) => {
-        this.chars = response.results;
-        this.totalPg = response.info.pages;
-      },
+    this.rickAndMortyServ.getChars(this.presentPg).subscribe({
+      next: (response: ApiResponse) => { this.chars = response.results; this.totalPg = response.info.pages; },
       error: (error: any) => { console.error('Error fetching characters:', error); }
     });
   }
 
-  page(page: number): void {
-    this.presentPg = page;
-    this.loadChars();
-  }
-
-  next(): void {
-    if (this.presentPg < this.totalPg) { this.presentPg++; this.loadChars(); }
-  }
+  page(page: number): void { this.presentPg = page; this.loadChars(); }
+  next(): void { if (this.presentPg < this.totalPg) { this.presentPg++; this.loadChars(); } }
+  prev(): void { if (this.presentPg > 1) { this.presentPg--; this.loadChars(); } }
 }
