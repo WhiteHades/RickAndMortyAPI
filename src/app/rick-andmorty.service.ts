01bb6598847e92a483a6242a28d14bce6b20d286
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2024-02-05 17:15:28 +0100

    Added full boilerplate, headers, footers, CSS and fonts (Home component only).

--- src/app/rick-andmorty.service.ts
+++ src/app/rick-andmorty.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable} from 'rxjs';
-import { Character } from './character.model';
 import { ApiResponse } from './character.model';
 
 @Injectable({
